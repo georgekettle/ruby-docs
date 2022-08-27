5c2cc9d2ccc23dba3b2b47793061cb8abeba3df7
george kettle <georgekettle.gk@gmail.com>
2022-08-27 12:47:52 +0200

    Seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if Rails.env == "development"
+	Section.destroy_all
+	Klass.destroy_all
+	Version.destroy_all
+end
+
+puts "Creating Ruby versions"
+versions = ['2.6', '2.7', '3.0', '3.1']
+versions.each do |version|
+	Version.create!(number: version)
+end
+puts "Finished creating Ruby versions"
+
+puts "Creating Ruby classes (Klass)"
+klasses = %w(String Integer Array Hash Symbol)
+Version.all.each do |version|
+	klasses.each do |klass_name|
+		puts "Creating #{klass_name} for ruby version #{version.number}"
+		klass = Klass.create!(version: version, name: klass_name)
+		ruby_klass = Object.const_get(klass_name)
+		puts "- Creating instance methods"
+		instance_methods = ruby_klass.instance_methods - Object.instance_methods
+		instance_methods.each do |instance_method|
+			puts "-- #{instance_method}"
+			Section.create!(name: instance_method, category: "instance_method", klass: klass)
+		end
+		puts "- Creating class methods"
+		class_methods = ruby_klass.methods(false)
+		class_methods.each do |class_method|
+			puts "-- #{class_method}"
+			Section.create!(name: class_method, category: "class_method", klass: klass)
+		end
+	end
+end
