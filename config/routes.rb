3646997e5ed9b252ecf17423b352d123b2a0b133
george kettle <georgekettle.gk@gmail.com>
2022-08-29 13:05:27 +0200

    Added custom route for Section show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "v/:version_id/:klass_name", to: "klasses#show", as: :klass, :version_id => /.*/
+  get "v/:version_id/:klass_name/:method", to: "sections#show", as: :section, :version_id => /.*/
 end
