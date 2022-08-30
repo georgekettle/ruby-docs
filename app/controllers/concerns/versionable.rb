8d5239f77dcf085b02884d530474a0c36d4950aa
george kettle <georgekettle.gk@gmail.com>
2022-08-30 17:19:43 +0200

    Set Current.version each controller action

---
+++ app/controllers/concerns/versionable.rb
@@ -0,0 +1,13 @@
+module Versionable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_current_version
+  end
+
+  def set_current_version
+    v_num = params[:version_number] || session[:version_number]
+    Current.version = v_num ? Version.find_by(number: v_num) : Version.first
+    session[:version_number] = Current.version.number
+  end
+end
