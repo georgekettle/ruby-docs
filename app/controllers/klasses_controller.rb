b9dee25a60402d64c24cd03d7e66d0fcbc79ac27
george kettle <georgekettle.gk@gmail.com>
2022-09-11 12:40:17 +0200

    Added breadcrumbs to sub nav on mobile

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -4,6 +4,8 @@ class KlassesController < ApplicationController
   def show
     @version = Version.find_by_number(params[:version_number])
     @klass = Klass.find_by(name: klass_name, version: @version)
+    breadcrumb @klass.name, "#"
+    breadcrumb "Overview", klass_url(@klass.version.number, @klass.name)
   end
 
   private
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,6 +5,8 @@ class SectionsController < ApplicationController
     @version = Version.find_by_number(params[:version_number])
     @klass = Klass.find_by(name: klass_name, version: @version)
     @section = Section.find_by(name: section_name, klass: @klass)
+    breadcrumb @klass.name, "#"
+    breadcrumb @section.name, klass_url(@klass.version.number, @klass.name)
   end
 
   private
