b6a2493d0f0a0d0c789b2691dcabfcc1e2e4d07a
george kettle <georgekettle.gk@gmail.com>
2022-09-27 17:57:46 +0100

    Implemented edit/update flow for version

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -9,4 +9,22 @@ class VersionsController < ApplicationController
     @version = Version.find(params[:id])
     redirect_to version_redirect_path(@version.number), status: :see_other
   end
+
+  def edit
+    @version = Version.find(params[:id])
+  end
+
+  def update
+    if @version.update(version_params)
+      redirect_to version_path(@version), status: :see_other, notice: "Version successfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def version_params
+    params.require(:version).permit(:number)
+  end
 end
