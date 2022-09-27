48eefe897f9c738806e24b0ca29e3a20ac046ca1
george kettle <georgekettle.gk@gmail.com>
2022-09-27 16:11:17 +0100

    Implemented edit/update flow for Klass

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -1,6 +1,7 @@
 class KlassesController < ApplicationController
   layout "documentation", only: [:show]
   skip_before_action :authenticate_user!, only: [:show]
+  before_action :set_klass, only: [:edit, :update]
 
   def show
     @version = Version.find_by_number(params[:version_number])
@@ -9,9 +10,28 @@ class KlassesController < ApplicationController
     breadcrumb "Overview", klass_url(@klass.version.number, @klass.name)
   end
 
+  def edit
+  end
+
+  def update
+    if @klass.update(klass_params)
+      redirect_to klass_url(@klass.version.number, @klass.name), status: :see_other, notice: "Successfully updated #{@klass.name}"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_klass
+    @klass = Klass.find(params[:id])
+  end
 
   def klass_name
     params[:klass_name]&.capitalize
+  end
+
+  def klass_params
+    params.require(:klass).permit(:name, :version_id, :summary, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get "v/:version_number/:klass_name", to: "klasses#show", as: :klass, :version_number => /[^\/]+/
   get "v/:version_number", to: "versions#show", as: :version, :version_number => /[^\/]+/
   get "search", to: "search#search", as: :search
+
+  resources :klasses, only: [:edit, :update]
 end
