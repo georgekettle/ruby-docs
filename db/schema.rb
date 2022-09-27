d61457f0f4a2dad4e7243a7aa56ecab59f32d5fa
george kettle <georgekettle.gk@gmail.com>
2022-09-27 16:02:26 +0100

    Added summary and content fields to Klass model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_26_171410) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_150115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_26_171410) do
     t.bigint "version_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "summary"
+    t.text "content"
     t.index ["version_id"], name: "index_klasses_on_version_id"
   end
 
