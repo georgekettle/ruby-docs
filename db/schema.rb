8d1482687087696ad2ff42faeb586a0e4169c5bf
george kettle <georgekettle.gk@gmail.com>
2022-10-11 19:44:59 +1100

    Added main menu boolean field to klass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_083107) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_084109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_083107) do
     t.datetime "updated_at", null: false
     t.string "summary"
     t.bigint "parent_id"
+    t.boolean "main_menu", default: false
     t.index ["parent_id"], name: "index_klasses_on_parent_id"
     t.index ["version_id"], name: "index_klasses_on_version_id"
   end
