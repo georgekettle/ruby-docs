851e09ac2ad500b219db8bc04167903c4346dfad
george kettle <georgekettle.gk@gmail.com>
2022-10-12 11:16:13 +1100

    Added dependent destroy to has_many children for Klass and updated length validation to 200 chars

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -5,10 +5,10 @@ class Klass < ApplicationRecord
   belongs_to :version
   has_many :sections, -> { order(name: :asc) }, dependent: :destroy
   belongs_to :parent, class_name: 'Klass', optional: true
-  has_many :children, foreign_key: 'parent_id', class_name: 'Klass'
+  has_many :children, foreign_key: 'parent_id', class_name: 'Klass', dependent: :destroy
 
   has_rich_text :content
 
   validates :name, presence: true
-  validates :summary, presence: true, length: { maximum: 140 }
+  validates :summary, presence: true, length: { maximum: 200 }
 end
