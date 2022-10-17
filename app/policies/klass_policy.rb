0170688d7e52bc3d94ad65ddd0b6d4735eb6f55a
george kettle <georgekettle.gk@gmail.com>
2022-10-17 13:30:32 +1100

    Added pundit policies for klasses

---
+++ app/policies/klass_policy.rb
@@ -0,0 +1,24 @@
+class KlassPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    admin?
+  end
+
+  def update?
+    admin?
+  end
+
+  def destroy?
+    admin?
+  end
+end
