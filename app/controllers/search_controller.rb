af9be5758b3d43e99e57551038e2dd6ca66071a3
george kettle <georgekettle.gk@gmail.com>
2022-10-17 17:26:27 +1100

    Added skip authorize for search functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   skip_before_action :authenticate_user!, only: [:search]
+  skip_after_action :verify_authorized, only: [:search]
 
   def search
     @hits = AlgoliaDocsIndexSearcher.new(params[:query]).call
