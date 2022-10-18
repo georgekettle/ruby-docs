a240eb6c55c52084f05eedd9d030369d9bb89107
george kettle <georgekettle.gk@gmail.com>
2022-10-18 21:35:16 +1100

    Added og images, favicon and icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+    site: 'ruby-docs.org',
+      title: 'Ruby Docs',
+      reverse: true,
+      separator: '|',
+      description: 'Better Ruby Documentation',
+      keywords: 'ruby, ruby docs, ruby documentation, ruby programming language',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.png') },
+        { href: image_url('icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'ruby-docs.org',
+        title: 'Ruby Docs',
+        description: 'Better Ruby Documentation', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('cover.png')
+      }
+    }
+  end
 end
