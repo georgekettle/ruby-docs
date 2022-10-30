c0bf3eaab4740d3651c6525077807c3f8916c7f7
george kettle <georgekettle.gk@gmail.com>
2022-10-30 12:02:00 +1100

    Added config for workers count with WEB_CONCURRENCY env var

--- config/puma.rb
+++ config/puma.rb
@@ -31,6 +31,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
